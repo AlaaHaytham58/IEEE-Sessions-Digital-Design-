03d105ae1a134ae2a6bf59eccef99f701c6698dc
AlaaHaytham58 <alaahaytham910@gmail.com>
2025-07-01 05:11:10 -0700

    Refactor mux modules: rename and correct output types for mux8bit and mux4

--- 8Mux.v
+++ 8Mux.v
@@ -1,18 +1,23 @@
-module 8bit-mux
-(
+module mux8bit (
     input wire [7:0] d, 
     input wire [2:0] sel, 
-    output reg y 
+    output wire y
 );
-4mux mux0 (
+
+wire y0, y1;
+
+mux4 mux0 (
     .d(d[3:0]),
     .sel(sel[1:0]),
     .y(y0)
 );
-4mux mux1 (
+
+mux4 mux1 (
     .d(d[7:4]),
     .sel(sel[1:0]),
     .y(y1)
 );
+
 assign y = (sel[2] == 0) ? y0 : y1; 
+
 endmodule
--- Session2/4-1Mux.v
+++ Session2/4-1Mux.v
@@ -1,13 +1,20 @@
-module 4mux(
- input wire d[3:0],
- input wire sel[1:0],
- output reg y[3:0]
+module mux4 (
+    input wire [3:0] d,
+    input wire [1:0] sel,
+    output wire y
 );
-wire and1,and2,and3,and4;
-and(and1,~sel[1],~sel[0],d[0]);
-and(and2,~sel[1],sel[0],d[1]);
-and(and3,sel[1],~sel[0],d[2]);
-and(and4,sel[1],sel[0],d[3]);
-or(y,and1,and2,and3,and4);
+
+wire and0, and1, and2, and3;
+wire not_sel0, not_sel1;
+
+not (not_sel0, sel[0]);
+not (not_sel1, sel[1]);
+
+and (and0, not_sel1, not_sel0, d[0]); 
+and (and1, not_sel1, sel[0], d[1]);   
+and (and2, sel[1], not_sel0, d[2]);   
+and (and3, sel[1], sel[0], d[3]);     
+
+or (y, and0, and1, and2, and3);
 
 endmodule
